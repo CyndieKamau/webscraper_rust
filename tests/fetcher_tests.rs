2c653b28e40b88a33a0f62fe1f5097f45d3325d5
CyndieKamau <cyndiekamaa@gmail.com>
2024-07-03 01:57:22 +0300

    chore: setup file structure and clippy

---
+++ tests/fetcher_tests.rs
@@ -0,0 +1,6 @@
+// Writing tests for the following cases :
+// Fetching a variety of URLs returns a valid response
+// Fetching deformed URLs returns an appropriate error
+// Test both http, https return appropriate responses
+
+
---
+++ tests/mod.rs
@@ -0,0 +1,3 @@
+pub mod fetcher_tests;
+pub mod processor_tests;
+pub mod integration_tests;
